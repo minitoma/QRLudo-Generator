83713a3043fdc6c64f5f4cc113406583439c3c87
etudiant <jules.leguy55@gmail.com>
2017-10-23 21:11:57 +0200

    Ajout de la classe QRCodeGenerator permettant de générer une image de QRCode à partir d'un objet QRCode

--- Controller/script.js
+++ Controller/script.js
@@ -1,17 +1,16 @@
 
 var qrCodeA = new QRCodeAtomique();
 
+
 qrCodeA.ajouterFichier("http://fichier.drive.google.com", "nomFichier");
 qrCodeA.ajouterFichier("http://fichier2.drive.google.com", "nomFichier2");
 qrCodeA.ajouterTexte("Ceci est un test");
 
 
 
-
 qrCodeA.ajouterAFamille("singes", 1);
 
-
-
+/*
 alert(qrCodeA.getDonnees());
 
 
@@ -21,5 +20,8 @@ alert(qrCodeA.getDonnees());
 
 qrCodeA.inverserOrdreContenu(1,2);
 
-
+*/
 alert(qrCodeA.getDonnees());
+
+
+QRCodeGenerator.generate('#affichageqr', qrCodeA);
