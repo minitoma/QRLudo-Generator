d9d254e5571def279074f01f33b46d82f420bc51
Unknown <jules.leguy55@gmail.com>
2017-11-09 08:46:33 +0100

    Modifications mineures

--- Controller/QRCodeGenerator.js
+++ Controller/QRCodeGenerator.js
@@ -69,7 +69,7 @@ class QRCodeGenerator{
 
       });
 
-/*
+
       //On génère une image jpg à partir du canvas et contenant les métadonnées du qrcode dans l'attribut Description exif
       var zerothIfd = {};
       zerothIfd[piexif.ImageIFD.ImageDescription] = qrcode.getMetadonnees();
@@ -86,6 +86,6 @@ class QRCodeGenerator{
 
       //On supprime le canvas initial
       canvas.parentNode.removeChild(canvas);
-*/
+
     }
 }
