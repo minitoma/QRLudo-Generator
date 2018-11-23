a2a5c5027af2dc7924877f63d6cbc423bf93984d
abdessabour9 <abdessabourharboul@gmail.com>
2018-11-24 00:39:11 +0100

    generation de dossier projetQrCODEquesrep

--- Model/QuestionReponse.js
+++ Model/QuestionReponse.js
@@ -13,7 +13,7 @@
  **/
 
 /*
- *Classe permettant de creer un projet de questions
+ *Classe permettant de creer un projet de questions_reponses
  */
 class Projet {
   //Constructeur d'un Projet
@@ -34,12 +34,28 @@ class Projet {
     this.projet.reponses.push(reponse)
   }
 
+  setName(nom) {
+    return this.projet.nom = nom;
+  }
+
+  getName() {
+    return this.projet.nom;
+  }
+
   getQuestions() {
     return this.projet.questions;
   }
 
   getReponses() {
     return this.projet.reponses;
+  }
+
+  getQuestionByIndex(indice) {
+    return this.projet.questions[indice];
+  }
+
+  getReponsesByIndex(indice) {
+    return this.projet.reponses[indice];
   }
 
   getDataString() {
@@ -52,7 +68,7 @@ class Projet {
  */
 class Question {
   //Constructeur d'une Question
-  constructor(title, reponsesUIDs = [], color) {
+  constructor(title, reponsesUIDs = [], color = '#00000') {
     this.qrcode = {
       id: new Date().getTime(),
       title: title,
@@ -62,19 +78,36 @@ class Question {
     };
   }
 
+  getName() {
+    return this.qrcode.id;
+  }
+
+  getId() {
+    return this.qrcode.id;
+  }
+
+  getTitle() {
+    return this.qrcode.title;
+  }
+
+  getReponsesUIDs() {
+    return this.qrcode.reponsesUIDs;
+  }
+
+  getReponseUIDByIndex(indice) {
+    return this.qrcode.reponsesUIDs[indice];
+  }
+
   getColor() {
     this.qrcode.color;
-  }
-  getDataString() {
-    return JSON.stringify(this.qrcode);
   }
 
   addReponse(reponseUid) {
     this.qrcode.reponsesUIDs.push(reponseUid);
   }
 
-  getName(){
-    return this.qrcode.id;
+  getDataString() {
+    return JSON.stringify(this.qrcode);
   }
 }
 
@@ -83,7 +116,7 @@ class Question {
  */
 class Reponse {
   //Constructeur d'une Reponse
-  constructor(title, color) {
+  constructor(title, color = '#00000') {
     this.qrcode = {
       id: new Date().getTime(),
       title: title,
@@ -92,10 +125,18 @@ class Reponse {
     };
   }
 
-  getName(){
+  getName() {
     return this.qrcode.id;
   }
-  
+
+  getId() {
+    return this.qrcode.id;
+  }
+
+  getTitle() {
+    return this.qrcode.title;
+  }
+
   getColor() {
     this.qrcode.color;
   }
@@ -105,27 +146,82 @@ class Reponse {
   }
 }
 
-// class QRCodeQuestionReponse {
-//
-//   // type = unique, xl
-//   // name = name of qrcode
-//   // data = an array which contains input (url file) provided from form
-//   // data = [
-//   //   {
-//   //     type: "file",
-//   //     url: "file url"
-//   //   }
-//   // ]
-//   // color = the qrcode color
-//   constructor(name = "", data = [], color = "") {
-//     super(name, data, color);
-//     this.qrcode.type = "questionre";
-//   }
-//
-// }
+
+/*
+ * Classe permettant à la vue d'interagir avec le controller.
+ * La vue instancie cette classe une seule fois à son initialisation et fait systématiquement appel à cette instance quand elle a besoin d'un traitement du controller.
+ */
+class QuesRepController {
+
+  constructor() {}
+
+
+  //Ajouter une nouvelle valeur a la liste deroulante
+  addNewValueToComboBox(new_val, selectid, modalIdToClose, array) {
+    if (new_val === "") return false; // si le champ est vide on sort
+    //sortir de la fonction si le champ entré existe deja
+    let existe = false;
+    $('select#' + selectid).find('option').each(function() {
+      if ($(this).text() === new_val) {
+        existe = true;
+        return;
+      }
+    });
+    if (existe) return false;
+    //Ajouter a la liste deroulante la nouvelle valeur
+    let nouvques = new Question(new_val);
+    array.push(nouvques);
+    $('#' + selectid).append($('<option>', {
+      val: nouvques.getId(),
+      text: new_val
+    }));
+    //fermer la pop-up
+    $("#" + selectid).val(nouvques.getId()).change();
+    $("#" + modalIdToClose + " .close").click();
+    return true;
+  }
+
+  //Ajouter une nouvelle valeur a un tableau
+  addNewValueToArray(new_val, my_array, modalIdToClose) {
+    if (new_val === "") return false; // si le champ est vide on sort
+    //sortir de la fonction si le champ entré existe deja
+    let existe = false;
+    $.each(my_array, function(i, val) {
+      if (val.getTitle() === new_val) {
+        existe = true;
+        return;
+      }
+    });
+    if (existe) return false;
+    //Ajouter au tableau la nouvelle valeur
+    my_array.push(new Reponse(new_val));
+
+    //fermer la pop-up
+    $("#" + modalIdToClose + " .close").click();
+    return true;
+  }
+
+  //Renvoie un Array des valeur d'une liste deroulante
+  selectOptionsValuesAsArray(selectId) {
+    let resArray = [];
+    $('select#' + selectId).find('option').each(function() {
+      resArray.push($(this).val());
+    });
+    return resArray;
+  }
+
+  clearModalForm(modal_id) {
+    $('#' + modal_id).find('form')[0].reset();
+  }
+
+
+}
+
+
 
 module.exports = {
   Projet,
   Reponse,
-  Question
+  Question,
+  QuesRepController
 };
--- js/QuestionReponse.js
+++ js/QuestionReponse.js
@@ -5,15 +5,10 @@
  * @Last modified time: 2018-11-23T12:56:20+01:00
  */
 
-
-
-var quesrepcontroller = new QuesRepController();
 var fs = require('fs');
 var pathname = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/') + 1);
-
 const path = require('path');
 let root = path.dirname(require.main.filename);
-
 const {
   FacadeController
 } = require(`${root}/Controller/FacadeController`);
@@ -21,13 +16,16 @@ const {
   QuestionReponse
 } = require(`${root}/Model/QuestionReponse`);
 
-let projet = new Projet("projet1");
-// var projet = quesrepcontroller.creerprojet("", [], []);
+let quesrepcontroller = new QuesRepController();
+let projet = new Projet("No_Name");
 
 
 
 
 $(document).ready(function() {
+
+  console.log("PathName : " + pathname);
+  console.log("Root : " + root);
 
   //Cacher le div des reponses par default
   $('#reponsesDivId').hide();
@@ -41,7 +39,6 @@ $(document).ready(function() {
   $("#addNewRepBtnId").click(function() {
     quesrepcontroller.clearModalForm('newReponseModalId');
   });
-  $('#questionTextAreaId').val()
 
   //Clear Choose Reponse form then load the combobox with all the reponses in the project.
   $("#addNewChooseRepBtnId").click(function() {
@@ -50,44 +47,39 @@ $(document).ready(function() {
       val: "norep",
       text: "---Selectionner Une Reponse---"
     }));
-    $.each(projet.reponses, function(i, val) {
+    $.each(projet.getReponses(), function(i, val) {
       $('#reponsesChooseSelectId').append($('<option>', {
-        val: val.id,
-        text: val.title
+        val: val.getId(),
+        text: val.getTitle()
       }));
     });
   });
 
   //Ajout d'une nouvelle question
   $("#addQuestionBtnId").click(function() {
-    //quesrepcontroller.clearModalForm('newQuestionModalId');
-    let question = new Question($('#questionTextAreaId').val(), [1, 2], '#00000');
-    projet.addQuestion(question);
-    // if (quesrepcontroller.addNewValueToComboBox($('#questionTextAreaId').val(), 'questionsId', 'newQuestionModalId', projet.questions)) {
-    $('#reponsesDivId').show();
-    // }
+    if (quesrepcontroller.addNewValueToComboBox($('#questionTextAreaId').val(), 'questionsId', 'newQuestionModalId', projet.getQuestions())) {
+      $('#reponsesDivId').show();
+    }
     console.log(projet);
   });
 
   //Ajout d'une nouvelle reponse
   $("#addReponseBtnId").click(function() {
-    //quesrepcontroller.clearModalForm('newReponseModalId');
-    let reponse = new Reponse("reponse 1", "#00000");
-    projet.addReponse(reponse);
-    // quesrepcontroller.addNewValueToArray($('#reponseTextAreaId').val(), projet.reponses, 'newReponseModalId');
+    quesrepcontroller.addNewValueToArray($('#reponseTextAreaId').val(), projet.getReponses(), 'newReponseModalId');
     console.log(projet);
   });
 
   //cet evenement permet l'affectation d'une reponse a la question selectionnée
   $("#chooseReponseBtnId").click(function() {
-    for (i = 0; i < projet.questions.length; i++) {
-      if (projet.questions[i].title === $("#questionsId option:selected").text()) {
-        for (j = 0; j < projet.questions[i].reponsesUIDs.length; j++) {
-          if (projet.questions[i].reponsesUIDs[j] === $("#reponsesChooseSelectId option:selected").val()) {
+    for (let ques_item of projet.getQuestions()) {
+      if (ques_item.getTitle() === $("#questionsId option:selected").text()) {
+        for (let rep_itemUid of ques_item.getReponsesUIDs()) {
+          if (rep_itemUid === $("#reponsesChooseSelectId option:selected").val()) {
             return;
           }
         }
-        projet.questions[i].reponsesUIDs.push($("#reponsesChooseSelectId option:selected").val());
+        ques_item.addReponse($("#reponsesChooseSelectId option:selected").val());
+        break;
       }
     }
     $("#reponsesDivLabelsId").append("<div class='form-inline'><label class='form-control control-label col-md-6' style='padding-top:10px;'>" + $("#reponsesChooseSelectId option:selected").text() + "</label>" +
@@ -104,13 +96,13 @@ $(document).ready(function() {
     else {
       $('#reponsesDivId').show(); // Si une autre valeur le div des reponses sera affiché
       $('#reponsesDivLabelsId').html('');
-      for (let ques_item of projet.questions) {
-        if ($(this).val() == ques_item.id) {
-          for (let repUid_item of ques_item.reponsesUIDs) {
-            for (let rep of projet.reponses) {
-              if (repUid_item == rep.id) {
-                $("#reponsesDivLabelsId").append("<div class='form-inline'><label class='form-control control-label col-md-6' style='padding-top:10px;'>" + rep.title + "</label>" +
-                  "<button id='" + rep.id + "' type='button' name='rep[]' class='btn btn-outline-success' onclick='deleteReponse($(this));'><i class='fa fa-trash-alt'></i></button></div>");
+      for (let ques_item of projet.getQuestions()) {
+        if ($(this).val() == ques_item.getId()) {
+          for (let repUid_item of ques_item.getReponsesUIDs()) {
+            for (let rep of projet.getReponses()) {
+              if (repUid_item == rep.getId()) {
+                $("#reponsesDivLabelsId").append("<div class='form-inline'><label class='form-control control-label col-md-6' style='padding-top:10px;'>" + rep.getTitle() + "</label>" +
+                  "<button id='" + rep.getId() + "' type='button' name='rep[]' class='btn btn-outline-success' onclick='deleteReponse($(this));'><i class='fa fa-trash-alt'></i></button></div>");
               }
             }
           }
@@ -121,176 +113,57 @@ $(document).ready(function() {
 
 
 
-
+  /* Methode qui genere un dossier avec un fichier json comportant tous les informations
+   sur les questions et reponse du projets ainsi que les qrcodes de ces questions_reponses*/
   $("#save").click(function() {
     var facade = new FacadeController();
+    projet.setName($("#projectId").val());
     console.log(projet);
-    // projet.nom = $("#projectId").val();
-    // let qrCodes_Generes = quesrepcontroller.dataToQRCodeJsonArray(projet);
-    // console.log(projet);
-    // console.log(qrCodes_Generes);
-    for (question of projet.getQuestions()) {
-
-      let dir = './directname';
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-      }
-
-      let div = document.createElement('div');
-      facade.genererQRCode($(div)[0], question);
-      saveQRCodeImage(div, question, "directname");
+    let dir = projet.getName();
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
     }
-
+    fs.writeFile(`${root}/${dir}/data.json`, projet.getDataString(), function(err) {
+      if (err) {
+        console.log("ERROR !!");
+      }
+    });
+    for (question of projet.getQuestions()) {
+      let div = document.createElement('div');
+      facade.genererQRCode(div, question);
+      saveQRCodeImage(div, question, projet.getName());
+      //element.parentNode.removeChild(div);
+    }
     for (reponse of projet.getReponses()) {
       let div = document.createElement('div');
       facade.genererQRCode(div, reponse);
-      saveQRCodeImage(div, reponse, "directname");
+      saveQRCodeImage(div, reponse, projet.getName());
+      //element.parentNode.removeChild(div);
     }
-
   });
 
   function saveQRCodeImage(div, qrcode, directoryName) {
-
+    //console.log("The DIV : " + div);
     let img = $(div).children()[0].src;
-
-    var data = img.replace(/^data:image\/\w+;base64,/, '');
-
+    //console.log("The IMG : " + img);
+    let data = img.replace(/^data:image\/\w+;base64,/, '');
+    //console.log("The DATA : " + data);
     fs.writeFile(`${root}/${directoryName}/${qrcode.getName()}.jpeg`, data, {
       encoding: 'base64'
     }, (err) => {
       if (err) throw err;
       console.log('The file has been saved!');
     });
-
   }
-
-
-
-
-  //----------------------------------------------------------------------------
-  console.log(pathname);
-  var nameFileProjects = "QA_ProjectNames_File.json";
-
-  $("#addQuestion").click(function() {
-    var myProject = new Projet(1, $('#project option:selected').text());
-    data = $('#formAddQuestion').serializeObject();
-    console.log(data);
-    var les_reponses = [];
-    for (i = 0; i < data.Reponse.length; i++) {
-      var myreponse = new Reponse(i, data.Reponse[i]);
-      les_reponses[i] = {
-        "reponse": myreponse,
-        "juste": document.getElementById("isEx" + (i + 1)).checked
-      };
-    }
-    var myQuestion = new Question(1, data.Question, [], myProject);
-    for (i = 0; i < les_reponses.length; i++) {
-      myQuestion.addReponse(les_reponses[i])
-    }
-    if (fs.existsSync(pathname + 'test.json')) {
-      var m = JSON.parse(fs.readFileSync('test.json').toString());
-      if (m === "") {
-        fs.writeFile("test.json", "[" + JSON.stringify(myQuestion) + "]", function(err) {
-          if (err) {
-            console.log("ERROR !!");
-          }
-        });
-      } else {
-        console.log(m);
-        m[m.length] = myQuestion;
-        fs.writeFile('test.json', JSON.stringify(m));
-      }
-    } else {
-      fs.writeFile("test.json", "[" + JSON.stringify(myQuestion) + "]", function(err) {
-        if (err) {
-          console.log("ERROR !!");
-        }
-      });
-    }
-    $("#nouvellequestion .close").click();
-  });
-
-  $("#project").change(function() {
-    $('#questions').find('option').remove().end();
-    if (fs.existsSync(pathname + "test.json")) {
-      var jsonl = JSON.parse(fs.readFileSync("test.json").toString());
-      $("#questions").append($.map(jsonl, function(o) {
-        if (o.project.nom === $("#project option:selected").text()) {
-          return $('<option/>', {
-            value: o.id,
-            text: o.title
-          });
-        }
-      }));
-    }
-  });
-
-  $("#questions").change(function() {
-    console.log("halii");
-    var myLoadedQuestion = $("#questions option:selected").text();
-    $("#modifyQuest").append(
-      "<label class='col-sm-12 control-label' style='color:#28a745;padding-top:10px;'>" + "Question</label>" +
-      " <div class='col-sm-10'>" + "<textarea class='form-control' id='question' rows='2' name='Question'>" + myLoadedQuestion + "</textarea></div>"
-    );
-    $("#questions").append($.map(jsonl, function(o) {
-      if (o.project.nom === $("#project option:selected").text()) {
-        return $('<option/>', {
-          value: o.id,
-          text: o.title
-        });
-      }
-    }));
-  });
 });
-
-
-function chargerProjects(nameFileProjects) {
-  if (fs.existsSync(pathname + nameFileProjects)) {
-    var jsonl = JSON.parse(fs.readFileSync(nameFileProjects).toString());
-    $("#project").append($.map(jsonl, function(o) {
-
-      return $('<option/>', {
-        value: o.id,
-        text: o.nom
-      });
-    }));
-  }
-}
-
-//pour ajouter une nvlle reponse
-function ajouterChampReponse() {
-  var Contenu = document.createElement('div');
-  Contenu.setAttribute("class", "form-inline");
-  Contenu.innerHTML = "<select id='reponsesId' name='reponsesName' class='form-control form-select-options col-md-6' style='margin-right:10px;'>" +
-    "<option value='norep'>---Selectionner Une Reponse---</option></select>" +
-    "<button id='deleteRepBtnId' type='button' class='btn btn-outline-success'><i class='fa fa-trash-alt'></i></button></div>";
-  document.getElementById('reponsesDivId').appendChild(Contenu);
-}
-
-
-$.fn.serializeObject = function() {
-  var o = {};
-  var a = this.serializeArray();
-  $.each(a, function() {
-    if (o[this.name] !== undefined) {
-      if (!o[this.name].push) {
-        o[this.name] = [o[this.name]];
-      }
-      o[this.name].push(this.value || '');
-    } else {
-      o[this.name] = this.value || '';
-    }
-  });
-  return o;
-};
 
 //Cette Fonction Supprime Une reponse a une question
 function deleteReponse(todelete) {
-  for (q of projet.questions) {
-    if (q.id == $("#questionsId option:selected").val()) {
-      for (i = 0; i < q.reponsesUIDs.length; i++) {
-        if (q.reponsesUIDs[i] == todelete.attr('id')) {
-          q.reponsesUIDs.splice(i, 1);
+  for (q of projet.getQuestions()) {
+    if (q.getId() == $("#questionsId option:selected").val()) {
+      for (i = 0; i < q.getReponsesUIDs().length; i++) {
+        if (q.getReponsesUIDs()[i] == todelete.attr('id')) {
+          q.getReponsesUIDs().splice(i, 1);
         }
       }
     }
