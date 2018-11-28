944322b06118574978179f6294ea8852c79608e4
ysalim <msalimyoussef@gmail.com>
2018-11-28 16:55:47 +0100

    modifs xl

--- js/scripts_xl.js
+++ js/scripts_xl.js
@@ -11,10 +11,10 @@
  $().ready(function() {
 
      // desactiver les boutons s'il y a rien à lire ou generer
-     $('#saveQRCode, #listenField, #stop, #preview, #annuler, #ajouterTexte, #showAudio').attr('disabled', true);
+     $('#saveQRCode,#preview, #annuler, #ajouterTexte, #showAudio').attr('disabled', true);
 
      //caché le button stop
-     document.getElementById("stop").style.display = "none";
+     // document.getElementById("stop").style.display = "none";
 
      //debut Preview
          // trigger preview qrcode action
@@ -36,7 +36,7 @@
                if(data.name == 'AudioName'){
                  let dataAudio = {
                                type: 'music',
-                               url: 'https://drive.google.com/open?id='+data.id,
+                               url: 'https://drive.google.com/uc?export=download&id='+data.id,
                                name: data.value
                              }
 
@@ -63,12 +63,6 @@
      //exporter le QR
      $('#saveQRCode').click(function(){ saveQRCodeImage(); });
 
-     // trigger save qr code image action
-     // $('#saveQRCode').click(e => {
-     //   console.log(e);
-     //   console.log(qrcode.getName());
-     //   saveQRCodeImage();
-     // });
 
      //debut annuler
        //btn annuler -> reinitialiser l'affichage
@@ -87,8 +81,8 @@
           }
 
         //buttons: play + stop -> zone QR
-        document.getElementById("listenField").style.display = "";
-        document.getElementById("stop").style.display = "none";
+        // document.getElementById("listenField").style.display = "";
+        // document.getElementById("stop").style.display = "none";
 
          //supprimer les textarea, inputs ..
          var divChamps = document.getElementById('cible');
@@ -97,31 +91,31 @@
           }
 
           //desactiver les buttons
-          $('#saveQRCode, #listenField, #stop, #preview, #annuler, #ajouterTexte, #showAudio').attr('disabled', true);
+          $('#saveQRCode, #preview, #annuler, #ajouterTexte, #showAudio').attr('disabled', true);
        });
      //fin annuler
 
        // sur clic du bouton Lire pour ecouter les textes saisis
-       $('button#listenField').click(function(){
-         document.getElementById("listenField").style.display = "none";
-         $('#stop').attr('disabled', false);
-         document.getElementById("stop").style.display = "";
-         //getForm(null);
-       });
+       // $('button#listenField').click(function(){
+       //   document.getElementById("listenField").style.display = "none";
+       //   $('#stop').attr('disabled', false);
+       //   document.getElementById("stop").style.display = "";
+       //   getForm(null);
+       // });
 
        // sur clic du bouton Stop
-       $('button#stop').click(function(){
-         document.getElementById("stop").style.display = "none";
-         document.getElementById("listenField").style.display = "";
-         //stopLecture();
-       });
+       // $('button#stop').click(function(){
+       //   document.getElementById("stop").style.display = "none";
+       //   document.getElementById("listenField").style.display = "";
+       //   stopLecture();
+       // });
 
        // ajouter un eventlistener sur playChamp pour lire le champ sur click du bouton
-      $('button.playChamp').click(function(event){
-        console.log(event.target);
-        var texte = event.target.parentNode.parentNode.parentNode.parentNode.childNodes[0].childNodes[0].value;
-        getForm(texte);
-      });
+      // $('button.playChamp').click(function(event){
+      //   console.log(event.target);
+      //   var texte = event.target.parentNode.parentNode.parentNode.parentNode.childNodes[0].childNodes[0].value;
+      //   getForm(texte);
+      // });
 
        //charger les fichiers audio dans le Modal 'listeMusic'
        remplirListeMusic();
@@ -137,17 +131,18 @@
    }
 
    //ajouter une nvlle legende (textarea) a chaque click sur button Texte (pour chaque textarea il faut rajouter à l'attribut class la valeur qrData class="... qrData")
-   function ajouterChampLegende(){
+   function ajouterChampLegende(valeur=null){
+
      var textareaLegende = document.createElement('div');
      textareaLegende.innerHTML = "<textarea class='form-control qrData' rows='3' name='legendeQR' placeholder='Mettre la légende'></textarea>"
                        +"<button type='button' class='btn btn-outline-success legendeQR-close-btn' onclick='supprimerChampLegende(this);'>"
                        +"<i class='fa fa-trash-alt'></i>"
-                       +"</button>"
-                       +"<button type='button' class='btn btn-outline-success legendeQR-close-btn playChamp'>"
-                       +"<i class='fa fa-play'></i>"
                        +"</button>";
      textareaLegende.setAttribute("class", "d-flex align-items-start legendeQR");
      textareaLegende.setAttribute("id", "legendeTexarea");
+     if(valeur != null){
+       textareaLegende.setAttribute("value",valeur);
+     }
      document.getElementById('cible').appendChild(textareaLegende);
 
    }
@@ -163,9 +158,6 @@
        inputSon.innerHTML = "<input type='text' id='"+url+"' name='AudioName' class='form-control qrData' value='"+nom+"' readonly>"
                          +"<button type='button' class='btn btn-outline-success legendeQR-close-btn' onclick='supprimerChampSon(this);'>"
                          +"<i class='fa fa-trash-alt'></i>"
-                         +"</button>"
-                         +"<button type='button' class='btn btn-outline-success legendeQR-close-btn'>"
-                         +"<i class='fa fa-play'></i>"
                          +"</button>";
        inputSon.setAttribute("class", "d-flex align-items-start legendeQR");
        inputSon.setAttribute("id", "inputAudio");
@@ -180,27 +172,6 @@
          $(e).parents('div#inputAudio').remove();
        }
 
-
-    // //supprimer les messages d'infos en haut de page
-    // function initMessages(){
-    //   var divMsg = document.getElementById('messages');
-    //    if(divMsg.firstChild)
-    //         divMsg.removeChild(divMsg.firstChild);
-    // }
-    //
-    //
-    // //message a afficher lors d'un : sauvegarde | Champ vide | Export
-    // //type: success | danger | warning
-    // function messageInfos(message,type){
-    //   initMessages();
-    //   var msg = document.createElement('div');
-    //   msg.innerHTML = message
-    //                     +"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
-    //   msg.setAttribute("class", "alert alert-"+type+" fade show");
-    //   msg.setAttribute("role", "alert");
-    //   document.getElementById('messages').appendChild(msg);
-    //
-    // }
 
     //remplir le Modal 'listeMusic' par des fichiers audio depuis le drive
     function remplirListeMusic(){
@@ -221,18 +192,3 @@
       }
 
     }
-
-     //creer+sauvegarder le fichier json correspond à un qrcode qui depasse la taille 500
-     function sauvegarderFichierJsonUnique(){
-
-       let now = new Date();
-       let nomFichier = now.getDay()+"-"+now.getMonth()+"-"+now.getFullYear()+"-"+now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
-       let path = "./QR-Unique/json/"+nomFichier+".json";
-       fs.writeFile(path, JSON.stringify(qrcode), (err) => {
-           if (err) {
-               console.error(err);
-               return;
-           };
-           messageInfos("votre fichier json est bien sauvegardé","success");
-       });
-     }
